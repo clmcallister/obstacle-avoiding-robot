c4ac4d8ffbb45dce7643aa3a76f6ab9ec328b772
Charlie L. McAllister <lamont152@gmail.com>
2016-03-13 01:01:03 -0500

    Add PING sensor left to right scan
    Making a change that will use a servo to move the PING ultrasonic sensor from center to left, left to center, and center to right. This will allow the robot to make decisions based on obstacles on the left and right sides of it.

--- Robot.ino
+++ Robot.ino
@@ -1,5 +1,6 @@
 #include <NewPing.h>
 #include <AFMotor.h>
+#include <Servo.h>
 
 #define PING_PIN 7
 #define MAX_DISTANCE 400
@@ -7,77 +8,78 @@
 #define LEFT_MOTOR_NUM 1
 #define RIGHT_MOTOR_NUM 2
 
-#define MAX_MOTOR_SPEED 160
+#define MAX_MOTOR_SPEED 180
 
-#define FAR_OBSTACLE_DIST_CM 30
-#define NEAR_OBSTACLE_DIST_CM 15
+#define OBSTACLE_DIST_CM 23
 
 AF_DCMotor motor1(LEFT_MOTOR_NUM);
 AF_DCMotor motor2(RIGHT_MOTOR_NUM);
+
+Servo pingServo;
 
 NewPing sonar(PING_PIN, PING_PIN, MAX_DISTANCE);
 
 boolean isFarObstacle = false;
 boolean isCloseObstacle = false;
+int robotCenter = 90;
 
 void setup() {
   Serial.begin(9600);
+
+  pingServo.attach(9);
+  pingServo.write(robotCenter);
 }
 
 void loop() {
+
+  moveRobot(MAX_MOTOR_SPEED, FORWARD);
+
   unsigned int distance_cm = sonar.ping() / US_ROUNDTRIP_CM;
 
   Serial.print("Distance from object is: ");
   Serial.print(distance_cm);
   Serial.print("\n");
 
-  if (distance_cm <= FAR_OBSTACLE_DIST_CM && distance_cm >= NEAR_OBSTACLE_DIST_CM) {
-    isFarObstacle = true;
-    isCloseObstacle = false;
-  } else if (distance_cm < NEAR_OBSTACLE_DIST_CM) {
-    isFarObstacle = false;
-    isCloseObstacle = true;
-  } else {
-    isFarObstacle = false;
-    isCloseObstacle = false;
-  }
+  if (distance_cm < OBSTACLE_DIST_CM) {
 
-  if (!isFarObstacle && !isCloseObstacle) {
-    Serial.println("No obstacles. Running motors...");
-    motor1.run(FORWARD);
-    motor1.setSpeed(MAX_MOTOR_SPEED);
-    motor2.run(FORWARD);
-    motor2.setSpeed(MAX_MOTOR_SPEED);
-    delay(500);
-    Serial.println("After delay");
+    stopRobot();
+
+    unsigned int left_object_distance_cm = scanLeft();
+    unsigned int right_object_distance_cm = scanRight();
+
+    if (left_object_distance_cm < OBSTACLE_DIST_CM && right_object_distance_cm < OBSTACLE_DIST_CM) {
+
+      // The area is blocked so turn around
+      moveRobot(MAX_MOTOR_SPEED, BACKWARD);
+      delay(1000);
+      stopRobot();
+      turnRobotLeft(MAX_MOTOR_SPEED, 5000);
+      moveRobot(MAX_MOTOR_SPEED, FORWARD);
+
+    } else if (left_object_distance_cm < right_object_distance_cm && right_object_distance_cm >= OBSTACLE_DIST_CM) {
+
+      // Obstacle on the left and we have a better chance by going right
+      turnRobotRight(MAX_MOTOR_SPEED, 1000); // Turn right to avoid object on the left
+
+    } else {
+
+      // Obstacle is on the right side and we have a better chance by going left
+      turnRobotLeft(MAX_MOTOR_SPEED, 1000);
+
+    }
+    
   }
   
-  if (isFarObstacle) {
-    Serial.println("Approaching obstacle. Stopping motors from forward movement.");
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    Serial.println("Turning left");
-    turnRobotLeft(MAX_MOTOR_SPEED, 1000);
-  }
-
-  if (isCloseObstacle) {
-    Serial.println("Very close to obstacle. Stopping motors from forward movement.");
-    motor1.run(RELEASE);
-    motor2.run(RELEASE);
-    delay(100);
-    Serial.println("Reversing");
-    moveRobot(MAX_MOTOR_SPEED, BACKWARD, 1000);
-    Serial.println("Now turning left");
-    turnRobotLeft(MAX_MOTOR_SPEED, 1000);
-  }
 }
 
-void moveRobot(int moveSpeed, int moveDirection, int duration) {
+void moveRobot(int moveSpeed, int moveDirection) {
   motor1.run(moveDirection);
   motor2.run(moveDirection);
   motor1.setSpeed(moveSpeed);
   motor2.setSpeed(moveSpeed);
-  delay(duration);
+}
+
+void stopRobot() {
   motor1.run(RELEASE);
   motor2.run(RELEASE);
 }
@@ -96,3 +98,24 @@ void turnRobotRight(int turnSpeed, int duration) {
   motor1.run(RELEASE);
 }
 
+unsigned int scanLeft() {
+  unsigned int distance_cm = 0;
+  for (int i = robotCenter; i <= 180; i++) {
+    pingServo.write(i);
+    delay(15);
+    distance_cm = sonar.ping() / US_ROUNDTRIP_CM;
+  }
+  pingServo.write(robotCenter);
+  return distance_cm;
+}
+
+unsigned int scanRight() {
+  unsigned int distance_cm = 0;
+  for (int i = robotCenter; i >= 0; i--) {
+    pingServo.write(i);
+    delay(15);
+    distance_cm = sonar.ping() / US_ROUNDTRIP_CM;
+  }
+  pingServo.write(robotCenter);
+  return distance_cm;
+}
